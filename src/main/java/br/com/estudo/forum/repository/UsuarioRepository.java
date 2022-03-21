615fd6aeaae2772307cf0383d903eee08cb3c218
harrissondutra <harrissondutra@gmail.com>
2022-03-21 18:00:07 -0300

    Início implementação de segurança.

---
+++ src/main/java/br/com/estudo/forum/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.estudo.forum.repository;
+
+import br.com.estudo.forum.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
